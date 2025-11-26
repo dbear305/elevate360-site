a331fad8ca8d64c500d1c2e7bc8d6024a2cc1512
danielberrieliv <danielberrieliv@gmail.com>
2025-11-26 05:34:18 -0500

    Finalize favicon implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,6 +85,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* FAVICONS – USE EXACTLY THESE THREE FILES */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
